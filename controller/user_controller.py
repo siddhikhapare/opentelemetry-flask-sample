321520203334f1425f55fcd10c0477af8bc9b514
siddhikhapare <81567515+siddhikhapare@users.noreply.github.com>
2022-08-23 23:47:30 +0530

    Update user_controller.py

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,7 +1,5 @@
 from app import app
 from flask import request
-#from http import HTTPStatus
-# classname and filename should be same as java file
 from model.user_model import user_model
 obj = user_model()
 
@@ -21,11 +19,3 @@ def getuserdetails(id):
 def deleteusers(id):
     return obj.deleteUsers_model(id)
 
-
-# @app.errorhandler(404)
-# def page_not_found(e):    
-#     return not_found(data=str(e), status_code=404)
-
-# @app.errorhandler(HTTPStatus.NOT_FOUND)
-# def page_not_found(e):
-#     return obj.not_found(message=str(e), status_code=HTTPStatus.NOT_FOUND)
